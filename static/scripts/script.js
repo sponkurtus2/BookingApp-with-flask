e0059566bf478f8e5685a888efe976f0cf658d10
sponkurtus2 <sponkurtus3@gmai.com>
2023-10-09 08:46:43 -0600

    Avance

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -8,6 +8,8 @@ function changeTheme() {
     document.documentElement.setAttribute('data-theme', newTheme)
 
     // Cambiar el color del marcado
+
+    // Marcado de la primera seccion
     const markElement = document.getElementById('custom-highlight')
     if (newTheme === 'light') {
         markElement.style.backgroundColor = 'rgba(0, 172, 193, 0.5)'; // Color de fondo para light theme
@@ -15,12 +17,23 @@ function changeTheme() {
         markElement.style.backgroundColor = 'rgba(67, 160, 71, 0.5)'; // Color de fondo para dark theme
     }
 
+    // Marcado de top countries
     const markCountryElement = document.getElementById('custom-highlight-country')
     if (newTheme === 'light') {
         markCountryElement.style.backgroundColor = 'rgba(0, 172, 193, 0.5)'; // Color de fondo para light theme
     } else {
         markCountryElement.style.backgroundColor = 'rgba(67, 160, 71, 0.5)'; // Color de fondo para dark theme
     }
+
+    // Marcado de top places
+    const markPlaceElement = document.getElementById('custom-highlight-places')
+    if (newTheme === 'light') {
+        markPlaceElement.style.backgroundColor = 'rgba(0, 172, 193, 0.5)'
+    } else {
+        markPlaceElement.style.backgroundColor = 'rgba(67, 160, 71, 0.5)'
+    }
 }
+
+
 
 
