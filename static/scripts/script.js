d4447fe4828be0b717a9a26b72f54ca3872d4dc5
sponkurtus2 <sponkurtus3@gmail.com>
2023-10-08 21:21:50 -0600

    Uploaded (not finished) booking app

---
+++ static/scripts/script.js
@@ -0,0 +1,26 @@
+// Change the color theme of the web
+
+function changeTheme() {
+    const currentTheme = document.documentElement.getAttribute('data-theme')
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light'
+
+    // Actualizar el elemento dentro de html
+    document.documentElement.setAttribute('data-theme', newTheme)
+
+    // Cambiar el color del marcado
+    const markElement = document.getElementById('custom-highlight')
+    if (newTheme === 'light') {
+        markElement.style.backgroundColor = 'rgba(0, 172, 193, 0.5)'; // Color de fondo para light theme
+    } else {
+        markElement.style.backgroundColor = 'rgba(67, 160, 71, 0.5)'; // Color de fondo para dark theme
+    }
+
+    const markCountryElement = document.getElementById('custom-highlight-country')
+    if (newTheme === 'light') {
+        markCountryElement.style.backgroundColor = 'rgba(0, 172, 193, 0.5)'; // Color de fondo para light theme
+    } else {
+        markCountryElement.style.backgroundColor = 'rgba(67, 160, 71, 0.5)'; // Color de fondo para dark theme
+    }
+}
+
+
